2a15d5d9a32df9c6d3c0471b3026d70983c131ee
luigicr95 <112467906+luigicr95@users.noreply.github.com>
2022-09-13 16:12:43 +0200

    Update

---
+++ js/script2.js
@@ -0,0 +1,38 @@
+let crowNames = "Unkindness";
+let crowText = null;
+let owlNames = "Parliament";
+let owlText = null;
+let larkNames = "bevy";
+let larkText = null;
+
+function getInfo() {
+  let crowText = document.getElementById("animals-text").value;
+  if (
+    crowText == crowNames &&
+    document.getElementById("animals").value == "crows"
+  ) {
+    alert("Well Done");
+  }
+}
+
+/*function getInfo() {
+  let owlText = document.getElementById("animals-text").value;
+  if (
+    owlText == owlNames &&
+    document.getElementById("animals").value == "owls"
+  ) {
+    alert("Well Done");
+  }
+}
+
+function getInfo() {
+  let larkText = document.getElementById("animals-text").value;
+  if (
+    larkText == larkNames &&
+    document.getElementById("animals").value == "larks"
+  ) {
+    alert("Well Done");
+  }
+}*/
+
+console.log(crowText == crowNames);
